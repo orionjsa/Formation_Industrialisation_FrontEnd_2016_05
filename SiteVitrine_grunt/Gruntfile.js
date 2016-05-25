db26e1e2ab323af5e7697bb0e0cab8254c40e20f
orionjsa <jsarasin@gmail.com>
2016-05-25 13:27:47 +0100

    copie des fonts & suppression rep dist et .tmp

--- SiteVitrine_grunt/Gruntfile.js
+++ SiteVitrine_grunt/Gruntfile.js
@@ -4,16 +4,45 @@
 
 module.exports = function(grunt){
     grunt.initConfig({
+        useminPrepare: {
+          html: 'index.html'
+        },
+
+        usemin: {
+            html: 'dist/index.html'
+        },
+
         copy: {
             build: {
                 files: [
                     {expand: true, src: 'index.html', dest: 'dist'},
-                    {expand: true, src: 'js/*', dest: 'dist'},
-                    {expand: true, src: 'css/*', dest: 'dist'},
+                    {expand: true, cwd: 'bower_components/bootstrap/fonts', src: '*', dest: 'dist/fonts/'},
                 ]
             }
-        }
+        },
+
+        clean: {
+            clean_dist: ["dist"],
+            clean_tmp: [".tmp"]
+        },
     });
 
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build', [
+        'clean:clean_dist',
+        'useminPrepare',
+        'copy:build',
+        'concat:generated',
+        'uglify:generated',
+        'cssmin:generated',
+        'usemin',
+        'clean:clean_tmp'
+    ]);
+    // en ligne de commande >grunt build pour generer le repertoire dist
 };
