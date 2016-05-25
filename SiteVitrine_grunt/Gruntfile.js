c5a046002ce208ffcf618a9744d1f2320c17bf50
orionjsa <jsarasin@gmail.com>
2016-05-25 11:06:03 +0100

    Création du repertoire dist et copie de index.html

--- SiteVitrine_grunt/Gruntfile.js
+++ SiteVitrine_grunt/Gruntfile.js
@@ -3,5 +3,14 @@
  */
 
 module.exports = function(grunt){
-    
+    grunt.initConfig({
+        copy: {
+            build: {
+                src: 'index.html',
+                dest: 'dist/index.html'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
 };
